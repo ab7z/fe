66f6b13046417bae14dd006932241fc428dedc55
Alan Bernstein <alan.bernstein@pm.me>
2024-03-17 00:31:26 +0100

    refactor: fetch content from cms
    The content is pre-fetched from cms based on selected locale.

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  i18n: {
+    locales: ['en', "fr", "de", "it", "fa"],
+    defaultLocale: 'en',
+  },
+  images: {
+    domains: ['localhost'],
+  },
 };
 
 export default nextConfig;
