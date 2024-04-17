56dd9abfede11dbcee7a83f72eb85eaf735bc86c
Alan Bernstein <alan.bernstein@pm.me>
2024-04-17 13:07:38 +0200

    test 1

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,32 @@
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import type { GetStaticProps, InferGetStaticPropsType } from "next";
 
 const inter = Inter({subsets: ["latin"]});
 
-export default function Home() {
+type CMS = {
+  url: string
+}
+
+export const getStaticProps = (() => {
+  const cmsURL = process.env.CMS_URL ?? "not defined";
+  const cms: CMS = { url: cmsURL };
+
+  return {
+    props: {
+      cms
+    }
+  }
+}) satisfies GetStaticProps<{
+  cms: CMS
+}>
+
+export default function Home({ cms }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <>
       <main className={`${styles.main} ${inter.className}`}>
         <h1>hello world</h1>
+        <a href={ cms.url }>{ cms.url }</a>
       </main>
     </>
   );
