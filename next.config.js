a1ea4c9d29737a942969405760ff5ce3c5179d7b
Alan Bernstein <alan.bernstein@pm.me>
2024-07-22 14:50:14 +0200

    fix(ci): add standalone to next conf

---
+++ next.config.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  i18n: {
+    locales: ["en"],
+    defaultLocale: "en",
+  },
+  images: {
+    domains: ["localhost"],
+  },
+  output: "standalone",
+}
+
+export default nextConfig
