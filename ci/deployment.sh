c2ffe1c4f869fde1d38f94b1b70126ee55afe658
Alan Bernstein <alan.bernstein@pm.me>
2024-07-23 10:23:07 +0200

    fix: try to fix the ci problem

--- ci/deployment.sh
+++ ci/deployment.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 echo "$TOKEN" | docker login ghcr.io -u "$USERNAME" --password-stdin
-
 SCRIPT_PATH="$(dirname "$0")"
 
 Cleanup () {
@@ -33,6 +32,26 @@ if ! docker service create \
   --replicas 3 \
   --network infra \
   --publish 80:80 \
+  --mount type=bind,readonly=true,source="$SCRIPT_PATH"/nginx.conf,target=/etc/nginx/nginx.conf \
+  nginx:stable; then
+  echo "proxy service error"
+  Cleanup
+  exit 1;
+fi
+
+echo "--------------------------------------------"
+echo "Start nginx proxy"
+echo "--------------------------------------------"
+
+docker service rm proxy || true
+sleep 10
+
+if ! docker service create \
+  --name proxy \
+  --replicas 3 \
+  --network cms \
+  --publish 80:80 \
+  --publish 443:443 \
   --mount type=bind,readonly=true,source="$SCRIPT_PATH"/nginx.conf,target=/etc/nginx/nginx.conf \
   nginx:stable; then
   echo "proxy service error"
